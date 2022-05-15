ec821e8181eab53cb060f1862cb5d1ca3f5ff51f
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-15 18:42:01 +0200

    make header much prettier to use filtering

--- src/components/header.js
+++ src/components/header.js
@@ -1,56 +1,16 @@
-import React, { Component } from "react";
+const Header = (props) => {
+
+    return (
+        <>
+            <header>
+                <div className='logo'> LOGO</div>
+                <input onChange={props.keywords}>
+                </input>
+            </header>
 
 
-class Header extends Component {
-
-    state = {
-        active: true,
-        name: 'Hadi',
-        title: 'The title is',
-        keyword: '',
-        count: 0
-
-    }
-    addOne() {
-        this.setState((state, props) => ({
-            count: state.count + 1
-
-        })
-        )
-
-    }
-    inputChangeHandeler(event) {
-        const value = event.target.value === '' ? true : false
-        this.setState({
-            active: value,
-            keyword: event.target.value,
-
-        })
-    }
-
-    render() {
-
-        return (
-            <>
-                <header style={{ background: `${this.state.active ? 'blue' : 'red'}` }} >
-                    <div className='logo'> LOGO</div>
-                    <input onChange={(e) => this.inputChangeHandeler(e)}>
-                    </input>
-                    <div>
-                        {this.state.count}
-                    </div>
-                    <button onClick={() => { this.addOne() }}>
-                        addOne
-                    </button>
-                </header>
-
-
-            </>
-        )
-
-    }
-
-
+        </>
+    )
 
 }
 
