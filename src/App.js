0b141e961fd757fd1b662caa92239be145be6413
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-16 19:01:45 +0200

    use useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = (props) => {
+  const [count, setCount] = useState(props.initialValue);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><h3>Count : {count}</h3>
+      <button onClick={() => ((setCount(count + 1)))}>add </button>
+      <button onClick={() => ((setCount(count - 1)))}>minus</button>
+      <button onClick={() => ((setCount(props.initialValue)))}>reset</button>
+    </>
   );
 }
 
